333490aa351cdbf467f3a0936f90826401ac2d2b
Kalaborative <kalaborative94@gmail.com>
2017-12-12 03:02:36 -0600

    Integrate PSQL

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from os import environ
 
 
 app = Flask(__name__)
+db_uri = "postgres://jewobnjxfznftb:062f6ed9eecd5b54c782c9c82252d6c4790d77e9f564096dbb88bd3865507d5e@ec2-54-235-123-153.compute-1.amazonaws.com:5432/d6j29morvgre4h"
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+db = SQLAlchemy(app)
+
+# Create our database model
+class User(db.Model):
+	__tablename__ = "users"
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(120), unique=True)
+	password = db.Column(db.String(120))
+
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
+
+	def __repr__(self):
+		return '<User {}>'.format(self.username)
 
 @app.route('/')
 def index():
